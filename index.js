13e04fbedf0b7a5a405520902afc2c8c1f29f9ae
Connor S <constroh@gmail.com>
2023-02-23 19:44:14 -0600

    Index.js Final

--- index.js
+++ index.js
@@ -1,7 +1,159 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generatePage = require("./src/generate-page");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const generatePage = require("./src/generate-page");
 const teamArray = [];
+
+// Prompt start //
+const displayMenu = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "Please select an option:",
+        choices: ["Add Engineer Profile", "Add Intern Profile", "Finish Building Team"],
+      },
+    ])
+    .then((menuOption) => {
+      switch (menuOption.menu) {
+        case "Add Engineer Profile":
+          promptEngineer();
+          break;
+        case "Add Intern Profile":
+          promptIntern();
+          break;
+        case "Finish Building Team":
+          htmlContent = generatePage(teamArray);
+          finishedTeam(htmlContent);
+          break;
+      }
+    });
+};
+
+// Manager Questions //
+const promptManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter manager's name:",
+      },
+      {
+        type: "number",
+        name: "id",
+        message: "Enter manager's ID number:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter manager's email:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter manager's office number:",
+      },
+    ])
+    .then((userInput) => {
+      const manager = new Manager(
+        userInput.name,
+        userInput.id,
+        userInput.email,
+        userInput.officeNumber
+      );
+      teamArray.push(manager);
+      displayMenu();
+    });
+};
+
+// Engineer Questions //
+const promptEngineer = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter engineer's name:",
+      },
+      {
+        type: "number",
+        name: "id",
+        message: "Enter engineer's ID number:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter engineer's email:",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter engineer's GitHub username:",
+      },
+    ])
+    .then((userInput) => {
+      const engineer = new Engineer(
+        userInput.name,
+        userInput.id,
+        userInput.email,
+        userInput.github
+      );
+      teamArray.push(engineer);
+      displayMenu();
+    });
+};
+
+// Intern Questions //
+const promptIntern = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter intern's name:",
+      },
+      {
+        type: "number",
+        name: "id",
+        message: "Enter intern's ID number:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter intern's email address:",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter the intern's school:",
+      },
+    ])
+    .then((userInput) => {
+      const intern = new Intern(
+        userInput.name,
+        userInput.id,
+        userInput.email,
+        userInput.school
+      );
+      teamArray.push(intern);
+      displayMenu();
+    });
+};
+
+const finishedTeam = (data) => {
+  fs.writeFile("./dist/index.html", data, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Webpage has been generated!");
+    }
+  });
+};
+
+// Call method to begin questions //
+promptManager();
--- src/generate-page.js
+++ src/generate-page.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 
 let profileCards = [];
 
-// Generate Manager //
+// Generate Manager layout //
 const generateManager = (manager) => {
   return `
-    <div class="bg-gray-100 rounded drop-shadow-[5px_5px_4px_rgba(76,76,76)] basis-1/4">
-      <div class="bg-bright-blue rounded-t">
-        <h2 class="text-white text-2xl font-semibold px-4 pt-3">${manager.name}</h2>
-        <h3 class="text-white text-xl font-semibold pl-4 pb-3"><i class="fa-solid fa-mug-hot pr-2"></i>Manager</h3>
+    <div class="bg-gray-100 rounded drop-shadow-[5px_5px_4px_rgba(76,76,76)] border-2 border-black basis-1/4">
+      <div class="bg-bright-purple rounded-t">
+        <h2 class="text-white text-2xl font-normal px-4 pt-3">${manager.name}</h2>
+        <h3 class="text-white text-xl font-normal pl-4 pb-3">Manager</h3>
       </div>
       <div class="text-sm">
         <div class="bg-white mx-4 mt-6 px-4 py-2 border-2 border-solid border-gray-200 rounded-t">
@@ -31,13 +31,13 @@ const generateManager = (manager) => {
     `;
 };
 
-// Generate Engineer //
+// Generate Engineer layout //
 const generateEngineer = (engineer) => {
   return `
-    <div class="bg-gray-100 rounded drop-shadow-[5px_5px_6px_rgba(76,76,76)] basis-1/4">
+    <div class="bg-gray-100 rounded drop-shadow-[5px_5px_6px_rgba(76,76,76)] border-2 border-black basis-1/4">
       <div class="bg-bright-blue rounded-t">
-        <h2 class="text-white text-2xl font-semibold px-4 pt-3">${engineer.name}</h2>
-        <h3 class="text-white text-xl font-semibold pl-4 pb-3"><i class="fa-solid fa-glasses pr-2"></i>Engineer</h3>
+        <h2 class="text-white text-2xl font-normal px-4 pt-3">${engineer.name}</h2>
+        <h3 class="text-white text-xl font-normal pl-4 pb-3">Engineer</h3>
       </div>
       <div class="text-sm">
         <div class="bg-white mx-4 mt-6 px-4 py-2 border-2 border-solid border-gray-200 rounded-t">
@@ -60,13 +60,13 @@ const generateEngineer = (engineer) => {
     `;
 };
 
-// Generate Intern //
+// Generate Intern layout //
 const generateIntern = (intern) => {
   return `
-    <div class="bg-gray-100 rounded drop-shadow-[5px_5px_6px_rgba(76,76,76)] basis-1/4">
-      <div class="bg-bright-blue rounded-t">
-        <h2 class="text-white text-2xl font-semibold px-4 pt-3">${intern.name}</h2>
-        <h3 class="text-white text-xl font-semibold pl-4 pb-3"><i class="fa-solid fa-user-graduate pr-2"></i>Intern</h3>
+    <div class="bg-gray-100 rounded drop-shadow-[5px_5px_6px_rgba(76,76,76)] border-2 border-black basis-1/4">
+      <div class="bg-bright-green rounded-t">
+        <h2 class="text-white text-2xl font-normal px-4 pt-3">${intern.name}</h2>
+        <h3 class="text-white text-xl font-normal pl-4 pb-3">Intern</h3>
       </div>
       <div class="text-sm">
         <div class="bg-white mx-4 mt-6 px-4 py-2 border-2 border-solid border-gray-200 rounded-t">
@@ -141,7 +141,7 @@ module.exports = (allInput) => {
     </head>
     <body>
       <header>
-        <div class="bg-coral text-white white text-center p-8 text-4xl drop-shadow-md">
+        <div class="text-center bg-coral text-white p-8 text-4xl">
           <p>My Team</p>
         </div>
       </header>
